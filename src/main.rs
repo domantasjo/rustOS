a72b7f25a9c293d08ba5e3679607176a5723b604
Domantas Jocas <domantas0216@gmail.com>
2026-02-12 22:29:39 +0200

    Added printing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print("Hello world");
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
